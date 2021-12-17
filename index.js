0d3cbaeb9e7df0766fc69332e02423172982327f
Kevin Rogers <kevinarogers1@gmail.com>
2021-12-17 13:03:28 -0500

    commit tree 00e77db7d3af710ad8c4ab618972d18943a64185

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+function superbowlWin(data) {
+    console.log(data)
+    let result = data.find((element) => element.result === 'W')
+    console.log(result)
+        
+    if (result) {
+    
+    return (result.year);}
+}
+
